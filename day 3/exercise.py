d35b7ff707b0c2e9f753f1192a806e83798a551a
Jeff Inman <jeffery.inman@hotmail.com>
2021-08-28 20:43:57 -0700

    completed ex day 3

--- day 3/exercise.py
+++ day 3/exercise.py
@@ -1,82 +1,82 @@
 # ITP Week 1 Day 3 Exercise
+
 # EASY
 
 
-
+# print(uppercase)
 # 1. loop through the lowercase and print each element
-    # print(low_letter)
+
 # 2. loop through the lowercase and print the capitalization of each element
 
-# print(uppercase)
-
-
-
-# uppercase.append(cap_letter)
-# print(uppercase)
 # MEDIUM
 
 # 1. create a new variable called uppercase with an empty list
+
 # 2. loop through the lowercase list
     # 2a. append the capitalization of each element to the uppercase list
-
-# uppercase = []
-# for low_letter in lowercase:
-#         uppercase.append(low_letter.capitalize())
-# print(uppercase)
-
 # HARD
 
 # A safe password has a minimum of (1) uppercase, (1) lowercase, (1) number, (1) special character.
-
-lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-uppercase = []
-special_char = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
-for letter in lowercase:
-    uppercase.append(letter.capitalize())  
-
 # 1. create the following variables and assign them Booleans as False
-has_uppercase = False
-has_lowercase = False
-has_number = False
-has_special_char = False
 
 # 2. loop through the string password (same as a list)
 # OR you can create a new list variable of the string password
 # using list(string) NOTE: assign it a new variable as such:
 # password_list = list(password) prior to looping.
+
+# Password - User input for flex goal
 # password = "MySuperSafePassword!@34"
+password = input("Please check if your password is valid: ")
 
-import re
-while True:
-    
-    input_password = input("Please enter a password")
-    is_valid = False
+# Manual check lists
+lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+uppercase = []
+number_list = []
+special_char = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
 
-    if len(input_password)<10 or len(input_password)>23:
-        print('password is between 10 - 23')
-        continue
-    elif not re.search(uppercase, input_password):
-        print('it should contain 1 uppercase')
-        # has_uppercase = True
-        continue
-    elif not re.search(lowercase, input_password):
-        print('it should contain 1 has_lowercase')
-        # has_lowercase = True
-        continue
-    elif not re.search('[1-9]', input_password):
-        print('it should contain 1 number')
-        # has_number = True
-        continue
-    elif not re.search(special_char, input_password):
-        print('it should contain 1 Special Character')
-        # has_special_char = True
-        continue
-    else:
-        is_valid = True
-        break
+# loops to populate manual lists
+for letter in lowercase:
+  uppercase.append(letter.capitalize())
 
-if(is_valid):
-    print("Valid Password")
+number_range = range(0, 10)
+for num in number_range:
+  number_list.append(str(num))
+
+# Validation booleans
+has_uppercase = False
+has_lowercase = False
+has_special_char = False
+has_number = False
+
+# logic for password checking
+if any(element1 in uppercase for element1 in password):
+    has_uppercase = True
+if any(element2 in lowercase for element2 in password):
+    has_lowercase = True
+if any(element3 in special_char for element3 in password):
+    has_special_char = True
+if any(element4 in number_list for element4 in password):
+    has_number = True
+
+
+# validation readout for output - Nightmare Flex Goal
+if has_uppercase != True:
+  print("Pasword Requires 1 Upper Case letter")
+elif has_lowercase != True:
+  print("Pasword Requires 1 Lower Case letter")
+elif has_special_char != True:
+  print("Pasword Requires 1 Special character")
+elif has_number != True:
+  print("Pasword Requires 1 Number")
+else:
+  print("Password is valid")
+
+# Original proof of life logic
+# print(has_uppercase)
+# print(has_lowercase)
+# print(has_special_char)
+# print(has_number)
+
 
 
 # 3. For each iteration of the loop, create a if statement
@@ -102,3 +102,4 @@ if(is_valid):
 # BONUS: update the password variable to take in an user input!
 
 # NIGHTMARE: in the final check, use another if statement to list why it isn't a strong password!
+
